6af1a1a6ed9af36458e68036e6c26f742f0c166c
MikeWolski <wolski17@gmail.com>
2024-09-25 11:57:08 -0400

    Created the library class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-# Define a class called book which represents a book in the library.
-class book:
+# Define a class called Book which represents a book in the library.
+class Book:
     def __init__(self, title, author, isbn, available):
         self.title = title
         self.author = author
@@ -24,8 +24,8 @@ class book:
         else:
             return False
 
-# Define a class called user which represents a user of the library.
-class user:
+# Define a class called User which represents a user of the library.
+class User:
     def __init__(self, name, user_id, borrowed_books):
         self.name = name
         self.user_id = user_id
@@ -48,3 +48,31 @@ class user:
         for book in self.borrowed_books:
             print(book)
 
+# Define a class called Library which represents a library, managing books and users.
+class Library:
+    def __init__(self, books, users):
+        self.books = books
+        self.users = users
+    # The add_book method is defined to add a book to the library.
+    def add_book(self, book):
+        self.books.append(book)
+    # The remove_book method is defined to remove a book from the library.
+    def remove_book(self, book):
+        self.books.remove(book)
+    # The add_user method is defined to add a user to the library.
+    def add_user(self, user):
+        self.users.append(user)
+    # The remove_user method is defined to remove a user from the library.
+    def remove_user(self, user):
+        self.users.remove(user)
+    # The borrow_book method is defined to facilitate borrowing by interacting with User and Book classes.
+    def borrow_book(self, user, book):
+        return user.borrow_book(book)
+    # The return_book method is defined to facilitate returning by interacting with User and Book classes.
+    def return_book(self, user, book):
+        return user.return_book(book)
+    # The list_available_books method is defined to show all books currently borrowed by a user.
+    def list_available_books(self):
+        for book in self.books:
+            if book.available:
+                print(book)
