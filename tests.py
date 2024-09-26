0d4c63547514464042627ba643dcb14ad24b798c
MikeWolski <wolski17@gmail.com>
2024-09-26 15:33:39 -0400

    working on the unit test

--- tests.py
+++ tests.py
@@ -3,10 +3,16 @@ from main import Book
 
 class TestBook(unittest.TestCase):
     def setUp(self):
-        self.book = Book(1, 2, 3, 4)
+        self.book = Book(a, a, a, True)
 
-    def test_borrow_book(self):
-        self.assertEqual(self.book.borrow_book())
+    def test_init(self):
+        self.assertEqual(self.book.title, a)
+        self.assertEqual(self.book.author, 2)
+        self.assertEqual(self.book.isbn, 3)
+        self.assertEqual(self.book.available, 4)
+
+#    def test_borrow_book(self):
+#        self.assertEqual(self.book.borrow_book())
 
 if __name__ == '__main__':
     unittest.main()
